36ab32973ffc11d9b42563a204e22c9cc4532eee
Brendik <92119591+Brendik@users.noreply.github.com>
2021-10-17 20:20:57 +0300

    Add files via upload

---
+++ ConsoleApp1/Caesar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Caesar
+    {
+        static string Library = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        static string Pass(string key, int n) 
+        {
+            while (key.Length<n)
+            { key += key; }
+            return key.Substring(0, n);     
+        }
+
+        public static string Encrypt(string text,string key) 
+        {
+            var pass = Pass(key, text.Length);
+            string intext = "";
+            int country = 0;        
+            for (int i = 0; i < text.Length; i++)
+            {
+                var LibrarIndex = Library.IndexOf(text[i]);
+                var codeIndex = Library.IndexOf(key[(i - country) % key.Length]);
+                if (LibrarIndex<0)
+                {
+                    intext += text[i];
+                    country++;
+                }
+                else 
+                {
+                    intext += Library[(LibrarIndex + codeIndex) % Library.Length];         
+                }            
+            }
+            return intext;
+            
+        }
+        public static string Dencrypt(string intext, string key) 
+        {
+            var pass = Pass(key, intext.Length);
+            string text = "";
+            int country = 0;
+            for (int i = 0; i < intext.Length; i++)
+            {
+                var LibrarIndex = Library.IndexOf(intext[i]);
+                var codeIndex = Library.IndexOf(key[(i - country) % key.Length]);
+                if (LibrarIndex < 0)
+                {
+                    text += intext[i];
+                    country++;
+                }
+                else
+                {
+                    text += Library[(LibrarIndex - codeIndex+Library.Length) % Library.Length];
+                }
+            }
+            return text;
+        }
+    }   
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести строку");
+            string text = Console.ReadLine();
+            Console.WriteLine("Ввести ключ");
+            string key = Console.ReadLine();
+            string intext = Caesar.Encrypt(text, key);
+            Console.WriteLine(intext);
+            string ditext = Caesar.Dencrypt(intext, key);
+            Console.WriteLine(ditext);
+            Console.ReadKey();
+        }
+    }
+}
